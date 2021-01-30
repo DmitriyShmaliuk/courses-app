280a2dde80240274627ae8a0027312d9b7843348
Dmitriy Shmaliuk <dmitriy.shmaliuk@gmail.com>
2021-01-30 16:29:56 +0200

    update: send registration email functionality

--- keys/index.js
+++ keys/index.js
@@ -2,5 +2,5 @@ module.exports = {
   SENDGRID_KEY: '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',
   MONGODB_URI: 'mongodb+srv://dshmaliuk:3esUB8CeogPvg2U7@cluster0.4znuh.mongodb.net/courses-shop?retryWrites=true&w=majority',
   SESSION_SECRET: 'some-secret-key',
-  BASE_EMAIL: 'course-nodejs@gmail.com',
+  BASE_EMAIL: 'dmitriy.shmaliuk@gmail.com',
 }
