ab186db64761adbab05b3c326525f1bea266ac09
Dmitriy Shmaliuk <dmitriy.shmaliuk@gmail.com>
2021-01-31 22:09:02 +0200

    add: production keys and hide development keys

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-  SENDGRID_KEY: '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',
-  MONGODB_URI: 'mongodb+srv://dshmaliuk:3esUB8CeogPvg2U7@cluster0.4znuh.mongodb.net/courses-shop?retryWrites=true&w=majority',
-  SESSION_SECRET: 'some-secret-key',
-  BASE_EMAIL: 'dmitriy.shmaliuk@gmail.com',
-  BASE_URL: `http://127.0.0.1:${process.env.PORT}`,
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod');
 }
+
+module.exports = require('./keys.dev');
---
+++ keys/keys.prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  SENDGRID_KEY: process.env.SENDGRID_KEY,
+  MONGODB_URI: process.env.MONGODB_URI,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  BASE_EMAIL: process.env.BASE_EMAIL,
+  BASE_URL: process.env.BASE_URL,
+}
